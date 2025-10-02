51d01492cef2f3075aeb8f2cc54ca97544725621
beckslange <beckslange96@gmail.com>
2025-10-01 21:12:30 -0600

    added in typwriter and js details

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//Challenge 2
+let challengeTwo = document.querySelector("#c2");
+challengeTwo.innerHTML = "Rebecca Lange";
+
+//Challenge 3
+new Typewriter("#c3", {
+  strings: "Rebecca Lange",
+  autoStart: true,
+  cursor: "",
+  delay: 60,
+});
